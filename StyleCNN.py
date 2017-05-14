fix loss computation

StyleCNN.py
@@ -69,20 +69,17 @@ class StyleCNN(object):
 
                 features = nn.Sequential(*layers)
                 if self.use_cuda:
-                    features.cude()
+                    features.cuda()
 
                 pastiche, content, style = features.forward(pastiche), features.forward(content), features.forward(style)
 
                 if "c" in losses:
-                    content_loss += self.loss(pastiche, content.detach())
+                    content_loss += self.loss(pastiche * self.content_weight, content.detach() * self.content_weight)
                 if "s" in losses:
                     pastiche_g, style_g = self.gram.forward(pastiche), self.gram.forward(style)
-                    style_loss += self.loss(pastiche_g, style_g.detach())
+                    style_loss += self.loss(pastiche_g * self.style_weight, style_g.detach() * self.style_weight)
 
                 start_layer = layer + 1
-
-            content_loss *= self.content_weight
-            style_loss *= self.style_weight
 
             total_loss = content_loss + style_loss
             total_loss.backward()

main.py
@@ -11,20 +11,19 @@ use_cuda = torch.cuda.is_available()
 dtype = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
 
 # Content and style
-style = image_loader("styles/picasso.jpg").type(dtype)
+style = image_loader("styles/starry_night.jpg").type(dtype)
 content = image_loader("contents/dancing.jpg").type(dtype)
 input = image_loader("contents/dancing.jpg").type(dtype)
 input.data = torch.randn(input.data.size()).type(dtype)
 
 kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-num_epochs = 300
+num_epochs = 31
 
 def main():
     style_cnn = StyleCNN(style, content, input)
 
     iter = 0
     for i in range(num_epochs):
-        #input = Variable(x_batch).type(dtype)
         pastiche = style_cnn.train()
 
         if iter % 10 == 0: